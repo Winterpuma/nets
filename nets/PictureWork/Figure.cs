245ac4f4bc74171817b05e44976d1a6c9b446d9e
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-05-04 17:40:29 +0300

    Create project and file recognition

---
+++ nets/PictureWork/Figure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureWork
+{
+    class Figure
+    {
+        private string path;
+        public Figure(string path)
+        {
+            this.path = path;
+        }
+    }
+}
---
+++ nets/PictureWork/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PictureWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles("../../../../src");
+            List<Figure> data = new List<Figure>();
+
+            foreach (string f in files)
+            {
+                data.Add(new Figure(f));
+            }
+
+        }
+    }
+}
